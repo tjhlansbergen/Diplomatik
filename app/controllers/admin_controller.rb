18d4fabe38a303059b85e8c4b39eb183a7ccd460
tjhlansbergen <takolansbergen@xs4all.nl>
2021-01-26 20:32:21 +0100

    Voeg aantal API gebruikers toe aan overzichtspagina

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -26,6 +26,7 @@ class AdminController < ActionController::Base
     # # toont views/admin/overview.html.erb en maakt de benodigde models beschikbaar voor de view
     def overview
         @customer_count = Customer.select{ |customer| customer.deleted == false }.count
+        @api_user_count = ApiUser.count
         @admin_count = AdminUser.count
         @log_entries = LogEntry.all.order(created_at: :desc)
     end
