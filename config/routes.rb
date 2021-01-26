4163491254d0050d25f5e769e9515565fbd67c4d
tjhlansbergen <takolansbergen@xs4all.nl>
2021-01-26 19:42:40 +0100

    Voeg routes toe voor API login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :customers
   resources :admin_users
+  resource :api_users, only: [:create]
+  post "/api/login", to: "api_users#login"
+  get "/api/auto_login", to: "api_users#auto_login"
   
   get 'admin_login', to: 'admin_sessions#new'
   post 'admin_login', to: 'admin_sessions#create'
