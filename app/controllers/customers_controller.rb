d178a26afced369c19c40d4b91cc98e02625b347
tjhlansbergen <takolansbergen@xs4all.nl>
2021-01-26 15:33:20 +0100

    Comments en header voor klant controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,22 +1,27 @@
+# customer_controller.rb - Tako Lansbergen 2020/01/26
+# 
+# Controller voor klantbeheer voor de Admin webinterface van de Diplomatik web-api 
+# overerft AdminController
+
 class CustomersController < AdminController
   include LogHelper   # ten behoeve van logging
 
-  # GET /customers
+  # toont views/customers/index.html.erb en laadt het benodigde model voor de view
   def index
     @customers = Customer.all
   end
 
-  # GET /customers/new
+  # toont views/customers/new.html.erb met nieuw geinitialiseerde klant
   def new
     @customer = Customer.new
   end
 
-  # GET /customers/1/edit
+  # toont naam-wijzigen formulier voor de gevraagde klant
   def edit
     @customer = Customer.find(params[:id])
   end
 
-  # POST /customers
+  # maakt een nieuw klant aan aan de hand van de opgegeven gegevens
   def create
     @customer = Customer.new(customer_params)
     @customer.deleted = false   # nieuwe klanten expliciet op niet-verwijderd zetten
@@ -26,23 +31,25 @@ class CustomersController < AdminController
       log LogEntry::INFORMATIONAL, "Klant #{@customer.name}, id #{@customer.id}, is aangemaakt door #{current_admin_user.username}"
       redirect_to customers_path
     else
+      # aanmaken niet gelukt, toon formulier opniew (met foutmelding
       render :new
     end
   end
 
-  # PATCH/PUT /customers/1
+  # werkt klantnaam bij voor de gevraagde gebruiker
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
-      # wijzigem gelukt, schrijft logentry en toon overzicht
+      # wijzigen gelukt, schrijft logentry en toon overzicht
       log LogEntry::INFORMATIONAL, "Klantnaam voor klant id #{@customer.id} is gewijzigd door #{current_admin_user.username}"
       redirect_to customers_path
     else
+      # wijzigen niet gelukt, toon formulier opnieuw (met foutmelding)
       render :edit
     end
   end
 
-  # DELETE /customers/1
+  # verwijdert (soft) een klant
   def destroy
     @customer = Customer.find(params[:id])
     # 'soft' delete de klant, (de namen van) historische klanten blijven behouden in de database
@@ -52,7 +59,7 @@ class CustomersController < AdminController
     redirect_to customers_path
   end
 
-  # Only allow a list of trusted parameters through.
+  # gedeelde methode voor verifieren van invoer
   def customer_params
     params.require(:customer).permit(:name)
   end
