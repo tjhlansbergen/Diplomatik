fd0022acf904ac287c2509e220f1bf7a3e9d0c7c
tjhlansbergen <takolansbergen@xs4all.nl>
2021-01-30 20:40:12 +0100

    Voeg functionaliteit voor toevoegen en verwijderen van API user in Admin UI toe

--- app/controllers/admin_api_users_controller.rb
+++ app/controllers/admin_api_users_controller.rb
@@ -1,10 +1,11 @@
 # admin_api_users_controller.rb - Tako Lansbergen 2020/01/28
 # 
-# Controller voor App gebruikers Admin beheerinterface van de Diplomatik web-api 
+# Controller voor API (App) gebruikers Admin beheerinterface van de Diplomatik web-api 
 # overerft van AdminController
 
 class AdminApiUsersController < AdminController
-      
+    include LogHelper   # ten behoeve van logging  
+
     # toont views/admin_api_users/index.html.erb en laadt het benodigde model voor de view
     def index
         @api_users = ApiUser.all
@@ -13,6 +14,33 @@ class AdminApiUsersController < AdminController
     # toont views/admin_api_users/new.html.erb met nieuw geinitialiseerde app gebruiker
     def new
         @api_user = ApiUser.new
+    end
+
+    # maakt een nieuw api-account aan aan de hand van de opgegeven gegevens
+    def create
+        result = @api_user = ApiUser.create(api_user_params)
+        if result.persisted?
+        # aanmaken gelukt, schrijft logentry en toon beheeraccounts overzicht
+        log LogEntry::INFORMATIONAL, "App gebruiker #{@api_user.username}, klant-id #{@api_user.customer_id}, is aangemaakt door #{current_admin_user.username}"
+        redirect_to admin_api_users_path
+        else
+        # aanmaken niet gelukt, toon formulier opniew (met foutmelding)
+        render :new
+        end
+    end
+
+    # verwijdert een api-account
+    def destroy
+        @api_user = ApiUser.find(params[:id])
+        @api_user.destroy
+        log LogEntry::INFORMATIONAL, "App gebruiker #{@api_user.username} is verwijderd door #{current_admin_user.username}"
+        redirect_to admin_api_users_path
+    end
+
+    # gedeelde methode voor verifieren van invoer
+    private
+    def api_user_params
+        params.require(:api_user).permit(:username, :password, :password_confirmation, :customer_id, :can_add_users)
     end
 
 end
--- app/models/api_user.rb
+++ app/models/api_user.rb
@@ -1,10 +1,16 @@
 # api_user.rb - Tako Lansbergen 2020/01/26
 # 
-# Model voor een API account
+# Model voor een App account
 # overerft van ApplicationRecord
 
 class ApiUser < ApplicationRecord
   belongs_to :customer    # foreign key naar de customer tabel
 
+  validates :username, presence: { message: "Geef een geldige gebruikersnaam op" }
+  validates :password, presence: { message: "Geef een geldig wachtwoord op" }, length: { minimum: 8, :message => "Wachtwoord is te kort (gebruik minimaal 8 tekens)" }
+  validates_confirmation_of :password, :message => "De opgegeven wachtwoorden komen niet overeen"
+  validate :new_password_is_different, on: :update
+
   has_secure_password     # ten behoeve van wachtwoord encryptie met bcrypt
+  attr_accessor :current_password     # sta een form-veld voor huidig wachtwoord toe wat geen property van het model is
 end
