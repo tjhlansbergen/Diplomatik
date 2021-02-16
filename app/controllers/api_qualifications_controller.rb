98fd94ac5abc760cc08dcfd301854226c682c2e5
tjhlansbergen <takolansbergen@xs4all.nl>
2021-02-16 15:08:11 +0100

    show, index, destroy & update endpoints voor de kwalificaties API

--- app/controllers/api_qualifications_controller.rb
+++ app/controllers/api_qualifications_controller.rb
@@ -6,18 +6,69 @@
 class ApiQualificationsController < ApiController
   include LogHelper   # ten behoeve van logging 
 
+  # toon een enkel item
+  def show
+    render json: Qualification.find(params[:id])
+  end
+
+  # toon alle items (behorende bij de klant, tenzij anders aangegeven)
+  def index
+    if(params[:all])
+      render json: Qualification.all
+    else
+    #render json:Qualification.includes(:customers).where(customers: { id: @api_user.customer_id}) 
+    end
+  end
+
+  # toevoegen van kwalificaties
   def create
     if result = Qualification.create!(qualification_params)
       # aanmaken gelukt, schrijf log en retourneer de aangemaakte kwalificatie, als indicatie dat de actie succesvol was
       log self.class.name, LogEntry::INFORMATIONAL, "Kwalificatie #{result.name} aangemaakt door #{@api_user.username}, klant id #{@api_user.customer_id}"
 
-      # vol koppeltabel
-      result.customers.push(@api_user.customer)
+      # vul koppeltabel
+      result.customers.append(@api_user.customer)
 
       render json: result
     else
       render_status :unprocessable_entity
     end
+  end
+
+  # verwijderen van kwalificaties
+  def destroy
+
+    # haal de gevraagde kwalificatie op
+    qualification_to_delete = Qualification.find(params[:id])
+
+    if qualification_to_delete
+      log self.class.name, LogEntry::INFORMATIONAL, "Kwalificatie #{qualification_to_delete.name} verwijderd voor klant id #{@api_user.customer_id} door #{@api_user.username}"
+
+      # verwijder uit koppeltabel
+      qualification_to_delete.customers.delete(@api_user.customer)
+
+      # als dit de laatste verwijzing naar de kwalificatie was, verwijderd dan ook de kwalificatie
+      if qualification_to_delete.customers.count == 0
+        log self.class.name, LogEntry::INFORMATIONAL, "Kwalificatie #{qualification_to_delete.name} verwijderd"
+        qualification_to_delete.destroy
+      end
+    end
+
+  end
+
+  # koppelen van bestaande kwalificaties
+  def update
+
+    # haal de gevraagde kwalificatie op
+    qualification_to_link = Qualification.find(params[:id])
+
+    # vul koppeltabel
+    if qualification_to_link
+      unless qualification_to_link.customers.include? @api_user.customer
+        qualification_to_link.customers.append(@api_user.customer)
+      end
+    end
+
   end
 
   # gedeelde methode voor verifieren van invoer
